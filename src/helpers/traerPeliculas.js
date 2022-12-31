26c95943674dd93d47253059ba3bf76dd3d7435b
jaen <jaenfigueroa03042000@gmail.com>
2022-12-31 15:13:39 -0500

    terminado

--- src/helpers/traerPeliculas.js
+++ src/helpers/traerPeliculas.js
@@ -1,5 +1,28 @@
+// import {ArrayPredeterminado} from '../helpers/ArrayPredeterminado'
+
 // TRAER LAS PELICULAS DE STORAGE///////////////////
 export const traerPeliculas = ()=>{
   let peliculas = JSON.parse(localStorage.getItem('peliculas'))
   return peliculas
 }
+
+
+// export const traerPeliculas = ()=>{
+//   let peliculas = JSON.parse(localStorage.getItem('peliculas'))
+
+//   console.log(peliculas)
+
+//   if (peliculas === null) {
+//     let otraLista  = []
+    
+//     otraLista = ArrayPredeterminado()
+
+//     console.log(otraLista);
+
+//     return otraLista
+//   } else{
+
+//     return peliculas
+//   }
+
+// }
