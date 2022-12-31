312667f1660f54ca1924e989dab0067cd7b92bbc
jaen <jaenfigueroa03042000@gmail.com>
2022-12-31 13:27:09 -0500

    inhabilitando el elemento de no hay resultados

--- src/componentes/Caja.js
+++ src/componentes/Caja.js
@@ -12,68 +12,55 @@ export const Caja = ({listaEstado, setListaEstado, setPeliEditar}) => {
     let resultado = traerPeliculas()
     setListaEstado(resultado)
 
+
+
+    new Sortable(listRef.current, {
+  
+      animation: 200,
+      chosenClass: 'pelicula--seleccionado',
+      dragClass: 'pelicula--invisible',
+      filter: '.contenedor__noResultados' ,
+      onEnd: function (evento) {
+
+        //comrobar que no sea la imagen de o hay resultados
+        if (evento.clone.className !== 'contenedor__noResultados' ) {
+
+          // console.log(evento.clone.className)
+          // Actualiza el array con el nuevo orden de los elementos
+          setListaEstado((lista)=>{
+            
+            //sacamos los indices
+            let indiceAntes = evento.oldIndex
+            let indiceNuevo = evento.newIndex
+  
+            //ACTUALIZAMOS EN ARREGLO
+            ////////////
+            let elemento = lista.splice(indiceAntes, 1);
+            let grupo = lista.splice(0, indiceNuevo)
+  
+            let actualizado = [...grupo, ...elemento, ...lista]
+            ////////////
+  
+            //guardar el nuevo arreglo el local storage
+            localStorage.setItem('peliculas', JSON.stringify([...actualizado]))
+  
+            //guardar el nuevo arreglo al estado
+            return [...actualizado]
+          })
+        }
+      } 
+    })
+
   },[setListaEstado, listRef])
 
-
-  useEffect(()=>{
-    console.log('se cargo')
-
-
-    console.log('largo:', listaEstado.length);
-
-      new Sortable(listRef.current, {
-  
-        animation: 200,
-        chosenClass: 'pelicula--seleccionado',
-        dragClass: 'pelicula--invisible',
-        filter: '.contenedor__noResultados' ,
-        onEnd: function (evento) {
-  
-          //comrobar que no sea la imagen de o hay resultados
-          if (evento.clone.className !== 'contenedor__noResultados' ) {
-  
-            // console.log(evento.clone.className)
-            // Actualiza el array con el nuevo orden de los elementos
-            setListaEstado((lista)=>{
-              
-              //sacamos los indices
-              let indiceAntes = evento.oldIndex
-              let indiceNuevo = evento.newIndex
-    
-              //ACTUALIZAMOS EN ARREGLO
-              ////////////
-              let elemento = lista.splice(indiceAntes, 1);
-              let grupo = lista.splice(0, indiceNuevo)
-    
-              let actualizado = [...grupo, ...elemento, ...lista]
-              ////////////
-    
-              //guardar el nuevo arreglo el local storage
-              localStorage.setItem('peliculas', JSON.stringify([...actualizado]))
-    
-              //guardar el nuevo arreglo al estado
-              return [...actualizado]
-            })
-          }
-        } 
-      })
-    
-  })
 
 
   const traerPeliculas = ()=>{
     let peliculas = JSON.parse(localStorage.getItem('peliculas'))
-    
     return peliculas
   }
 
-
   //////////////////////
-
-
-
-
-
   return (
     <div className='contenedor-peliculas' ref={listRef}>
           { listaEstado !== null && listaEstado.length > 0 ?
