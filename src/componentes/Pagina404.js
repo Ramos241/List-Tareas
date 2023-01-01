5363f5bea57c82b65cd8030f7ff31822e7d0979c
jaen <jaenfigueroa03042000@gmail.com>
2022-12-31 20:13:19 -0500

    terminado

--- src/componentes/Pagina404.js
+++ src/componentes/Pagina404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Imagen404 from '../assets/Imagen404.js'
 
 export const Pagina404 = () => {
@@ -9,7 +10,7 @@ export const Pagina404 = () => {
         <h1 className='caja404__titulo'>¡Ups! Página no encontrada</h1>
         <p className='caja404__parrafo'>
           La página a la que intenta acceder no existe o ha sido movida.</p>
-        <button className='caja404__boton'>Ir a la página de inicio</button>
+        <Link className='caja404__boton' to={'/inicio'}>Ir a la página de inicio</Link>
       </div>
     </div>
   )
